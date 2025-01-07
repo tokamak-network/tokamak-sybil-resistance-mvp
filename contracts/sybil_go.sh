20d7b9714f799feb19d7b26e0f7580cbebfa30d5
DarkKnight3074 <aryan@tokamak.network>
2025-01-07 13:45:21 +0530

    feat: add script to generate .go file

---
+++ contracts/sybil_go.sh
@@ -0,0 +1,11 @@
+# Build the project and generate the necessary artifacts
+forge build
+
+# Compile the Solidity contracts and generate binary files (bin)
+forge compile --extra-output-files bin
+
+# Extract the ABI from the JSON file and save it as a .abi file
+jq .abi out/mvp/Sybil.sol/Sybil.json > out/mvp/Sybil.sol/Sybil.abi
+
+# Use abigen to generate a Go package from the ABI and binary, and save it in the abi folder
+abigen --abi=out/mvp/Sybil.sol/Sybil.abi --bin=out/mvp/Sybil.sol/Sybil.bin --pkg=main --out=abi/sybil.go

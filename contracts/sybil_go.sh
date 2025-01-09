5bd109499d6e39a0e87817fced315cc7df02f6d8
DarkKnight3074 <aryan@tokamak.network>
2025-01-09 18:27:58 +0530

    fix: file path in sybil_go.sh

--- contracts/sybil_go.sh
+++ contracts/sybil_go.sh
@@ -8,4 +8,4 @@ forge compile --extra-output-files bin
 jq .abi out/mvp/Sybil.sol/Sybil.json > out/mvp/Sybil.sol/Sybil.abi
 
 # Use abigen to generate a Go package from the ABI and binary, and save it in the abi folder
-abigen --abi=out/mvp/Sybil.sol/Sybil.abi --bin=out/mvp/Sybil.sol/Sybil.bin --pkg=sybil --out=abi/sybil.go
+abigen --abi=out/mvp/Sybil.sol/Sybil.abi --bin=out/mvp/Sybil.sol/Sybil.bin --pkg=sybil --out=../sequencer/eth/contracts/sybil.go

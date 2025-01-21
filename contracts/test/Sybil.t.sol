1d3ec06a74b2a2c6e8594d6d53cd6cc15fe02762
shailu-s <emailid462@gmail.com>
2025-01-22 02:18:12 +0530

    Added script to emit event from the contract

--- contracts/test/Sybil.t.sol
+++ contracts/test/Sybil.t.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8 .23;
 
 import "forge-std/Test.sol";
-import "../src/Sybil.sol";
+import "../src/sybil.sol";
 import "./utils/Constants.sol";
 import "./types/TransactionTypes.sol";
 import "../src/VerifierRollupStub.sol";

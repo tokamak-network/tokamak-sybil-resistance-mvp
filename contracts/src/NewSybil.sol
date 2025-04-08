f7de440d4726abeef9971aa9b32d695bc4e2f375
DarkKnight3074 <aryan@tokamak.network>
2025-04-08 17:52:10 +0530

    update _MAX_TXNS to 73

--- contracts/src/NewSybil.sol
+++ contracts/src/NewSybil.sol
@@ -17,7 +17,7 @@ contract NewSybil is Initializable, AccessControlUpgradeable, IMVPSybil, MVPSybi
 
     uint48 constant _EXPLODE_AMOUNT = (1 << 50);
     uint256 constant _TXN_TOTALBYTES = 73; // Total bytes per transaction
-    uint256 constant _MAX_TXNS = 1000; // Max transactions per batch
+    uint256 constant _MAX_TXNS = 256; // Max transactions per batch
     uint256 constant _LIMIT_LOADAMOUNT = (1 << 128); // Max loadAmount per call
     uint256 constant _RFIELD = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");

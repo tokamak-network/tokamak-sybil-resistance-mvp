f0cbb1b5a774e9de767d037eb4359f8aea7e5686
DarkKnight3074 <aryan@tokamak.network>
2025-04-23 18:02:01 +0530

    feat: add testInvalidVouchWithReceiverHasZeroBalance test

--- contracts/test/NewSybil.t.sol
+++ contracts/test/NewSybil.t.sol
@@ -243,4 +243,15 @@ contract MvpTest is Test {
         sybil.vouch(address(0x123));
     }
 
+    function testInvalidVouchWithReceiverHasZeroBalance() public {
+        vm.prank(address(this));
+        sybil.deposit{
+            value: 1 ether
+        }();
+
+        vm.expectRevert(INewSybil.ReceiverHasZeroBalance.selector);
+        vm.prank(address(this));
+        sybil.vouch(address(0x123));
+    }
+
 }

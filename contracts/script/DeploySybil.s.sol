f4a2b9b4c5c1d30739cd27631b3b8eb593af7a4f
DarkKnight3074 <aryan@tokamak.network>
2025-05-07 16:14:42 +0530

    feat: update the deploy and test script

--- contracts/script/DeploySybil.s.sol
+++ contracts/script/DeploySybil.s.sol
@@ -1,38 +1,38 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.23;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.23;
 
-import "forge-std/Script.sol";
-import { NewSybil } from "contracts/src/NewSybil.sol";
-import {Verifier} from "../src/Verifier.sol";
+// import "forge-std/Script.sol";
+// import {Sybil} from "contracts/src/Sybil.sol";
+// import {Verifier} from "../src/Verifier.sol";
 
-contract FunctionScript is Script {
+// contract FunctionScript is Script {
 
-    function run() external {
-        address verifier = vm.envAddress("VERIFIER");
-        uint256 maxTx = vm.envUint("MAXTX");
-        uint256 nLevel = vm.envUint("NLEVEL");
+//     function run() external {
+//         address verifier = vm.envAddress("VERIFIER");
+//         uint256 maxTx = vm.envUint("MAXTX");
+//         uint256 nLevel = vm.envUint("NLEVEL");
 
-        // Specify Poseidon contract addresses
-        address poseidon2Elements = vm.envAddress("POSEIDON2ELEMENTS");
-        address poseidon3Elements = vm.envAddress("POSEIDON3ELEMENTS");
-        address adminRole = msg.sender;
+//         // Specify Poseidon contract addresses
+//         address poseidon2Elements = vm.envAddress("POSEIDON2ELEMENTS");
+//         address poseidon3Elements = vm.envAddress("POSEIDON3ELEMENTS");
+//         address adminRole = msg.sender;
 
-        vm.startBroadcast();
-        // Deploy the Sybil contract
-        NewSybil sybilContract = new NewSybil();
+//         vm.startBroadcast();
+//         // Deploy the Sybil contract
+//         Sybil sybilContract = new Sybil();
 
-        // Calling initialize at the time of deployment
-        sybilContract.initialize(
-            verifier,
-            maxTx,
-            nLevel,
-            poseidon2Elements,
-            poseidon3Elements,
-            adminRole
-        );
+//         // Calling initialize at the time of deployment
+//         sybilContract.initialize(
+//             verifier,
+//             maxTx,
+//             nLevel,
+//             poseidon2Elements,
+//             poseidon3Elements,
+//             adminRole
+//         );
 
-        vm.stopBroadcast();
+//         vm.stopBroadcast();
 
-        console2.log("Sybil contract is deployed at:", address(sybilContract));
-    }
-}
+//         console2.log("Sybil contract is deployed at:", address(sybilContract));
+//     }
+// }

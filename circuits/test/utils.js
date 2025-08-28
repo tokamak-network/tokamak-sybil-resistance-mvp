fa83d92c256be4e50e3bb089f3ea2537744a1f7e
Jeff Chung <sh1001309@gmail.com>
2025-08-28 18:06:04 +0800

    refactor: update generateCircuit function to use publicInputs and improve error handling

--- circuits/test/utils.js
+++ circuits/test/utils.js
@@ -7,46 +7,56 @@ const { wasm: tester } = require("circom_tester");
  * @param {string} circuitName - Name of the circuit template
  * @param {string} circuitPath - Path to the circuit file (relative to circuits directory)
  * @param {Object} options - Configuration options
- * @param {string[]} options.publicOutputs - Array of public output signal names
+ * @param {string[]} options.publicInputs - Array of public input signal names (default: empty)
  * @param {string} options.templateParams - Parameters to pass to the template (e.g., "(3, 2)")
  * @param {string} options.includePath - Additional include path for dependencies
  * @returns {Object} - Object containing circuit instance and cleanup function
  */
 async function generateCircuit(circuitName, circuitPath, options = {}) {
     const {
-        publicOutputs = ["root"],
+        publicInputs = [],
         templateParams = "",
         includePath = "../circuits"
     } = options;
 
     // Create the circuit source code
+    const publicList = publicInputs.length > 0 ? `{public [${publicInputs.join(", ")}]}` : "";
     const circuitSrc = `
         pragma circom 2.0.0;
         include "${circuitPath}";
-        component main{public [${publicOutputs.join(", ")}]} = ${circuitName}${templateParams};
+        component main${publicList} = ${circuitName}${templateParams};
     `;
 
     // Create temporary file path
     const circuitTmpPath = path.join(__dirname, `${circuitName}-temp.circom`);
     fs.writeFileSync(circuitTmpPath, circuitSrc, "utf8");
 
-    // Load the circuit
-    const circuit = await tester(circuitTmpPath, {
-        reduceConstraints: false,
-        include: path.join(__dirname, includePath)
-    });
-    await circuit.loadConstraints();
+    try {
+        // Load the circuit
+        const circuit = await tester(circuitTmpPath, {
+            reduceConstraints: false,
+            include: path.join(__dirname, includePath)
+        });
+        await circuit.loadConstraints();
 
-    // Return circuit and cleanup function
-    return {
-        circuit,
-        circuitTmpPath,
-        cleanup: () => {
-            if (fs.existsSync(circuitTmpPath)) {
-                fs.unlinkSync(circuitTmpPath);
+        // Return circuit and cleanup function
+        return {
+            circuit,
+            circuitTmpPath,
+            // cleanup function to delete the tmp files
+            cleanup: () => {
+                if (fs.existsSync(circuitTmpPath)) {
+                    fs.unlinkSync(circuitTmpPath);
+                }
             }
+        };
+    } catch (error) {
+        // Clean up temp file even if circuit loading fails
+        if (fs.existsSync(circuitTmpPath)) {
+            fs.unlinkSync(circuitTmpPath);
         }
-    };
+        throw error;
+    }
 }
 
 /**

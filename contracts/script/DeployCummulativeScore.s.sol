7621de3c236a8cdc8afec5936137d6d84da4c58d
DarkKnight3074 <aryan@tokamak.network>
2025-09-11 12:57:00 +0530

    feat: update the CumulativeScore contract deploy script

--- contracts/script/DeployCummulativeScore.s.sol
+++ contracts/script/DeployCummulativeScore.s.sol
@@ -8,7 +8,7 @@ contract DeployStakingInterface is Script {
 
     function run() external {
         // Get required addresses from environment variables
-        address depositManager = vm.envAddress("DEPOSIT_MANAGER");
+        address seigManager = vm.envAddress("Seig_Manager");
         address sybilContract = vm.envAddress("SYBIL_CONTRACT_ADDRESS");
         address adminRole = msg.sender;
 
@@ -16,7 +16,7 @@ contract DeployStakingInterface is Script {
         
         // Deploy the Staking contract
         CumulativeScore stakingContract = new CumulativeScore(
-            depositManager,
+            seigManager,
             sybilContract,
             adminRole
         );
@@ -24,7 +24,7 @@ contract DeployStakingInterface is Script {
         vm.stopBroadcast();
 
         console2.log("Staking contract is deployed at:", address(stakingContract));
-        console2.log("DepositManager address:", depositManager);
+        console2.log("SeigManager address:", seigManager);
         console2.log("Sybil contract address:", sybilContract);
         console2.log("Admin role granted to:", adminRole);
     }
